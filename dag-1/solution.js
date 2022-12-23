433bfb57dbc2056a3b9951e638b28045b5b5cbf6
speelgren <97473666+speelgren@users.noreply.github.com>
2022-12-23 01:02:41 +0100

    Update solution.js

--- dag-1/solution.js
+++ dag-1/solution.js
@@ -1,22 +1,22 @@
 const fs = require("fs");
 
-fs.readFile('input.txt', 'utf-8', (error, data) => {
-    if (error) throw error;
+fs.readFile('input.txt', 'utf-8', ( error, data ) => {
+    if ( error ) throw error;
 
-    let elves = data.replace(/\r/g, "").trim().split("\n\n");
+    let elves = data.trim().split("\n\n");
 
     let calories = elves.map(( elf ) => {
 
         let calories = elf.split("\n").map(Number);
-        return calories.reduce((prev, curr) => prev + curr, 0);
+        return calories.reduce(( prev, curr ) => prev + curr, 0);
     });
 
     //part one
     console.log("Max: " + Math.max(... calories));
 
     //part two
-    let sum = calories.sort((a, b) => b - a)
+    let sum = calories.sort(( a, b ) => b - a)
         .slice(0, 3)
-        .reduce((prev, curr) => prev + curr, 0);
+        .reduce(( prev, curr ) => prev + curr, 0);
     console.log("Sum of top 3: " + sum);
 });
